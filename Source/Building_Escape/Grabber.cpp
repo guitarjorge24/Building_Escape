68ab5ae59e7ada807ba8fef912d2bcfbfa4ce5de
Jorge Luque <guitarjorge24@outlook.com>
2020-07-01 02:33:29 -0400

    Created a new custom Game Mode blueprint based on the base Game Mode. Created custom pawn BP based on default pawn and set it as the default pawn class on the Game Mode. Created Grabber component and attached to custom pawn BP which logs player viewpoint every frame.

---
+++ Source/Building_Escape/Grabber.cpp
@@ -0,0 +1,39 @@
+// Copyright Jorge Luque 2020
+
+#include "GameFramework/PlayerController.h"
+#include "Engine/World.h"
+#include "Grabber.h"
+
+#define OUT
+
+// Sets default values for this component's properties
+UGrabber::UGrabber()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+void UGrabber::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UE_LOG(LogTemp, Warning, TEXT("Grabber component initialized"));
+}
+
+void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	FVector PlayerViewPointLocation;
+	FRotator PlayerViewPointRotation;
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
+		OUT PlayerViewPointLocation, 
+		OUT PlayerViewPointRotation
+	);
+
+	UE_LOG(LogTemp, Warning, TEXT(" \nPlayerViewPointLocation: %s \nPlayerViewPointRotation: %s"), 
+		*PlayerViewPointLocation.ToString(), 
+		*PlayerViewPointRotation.ToString()
+	);
+}
+
